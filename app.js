8b9d11fb3e361671d2a7e415be51bd8e6b2766ad
IT-YASH <pyash7646@gmail.com>
2023-09-19 19:06:04 +0530

    upload API online

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerJsdocs = YAML.load('./swagger.yamljs')
+const app= express()       
+app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerJsdocs))
+
+
+
+app.get("/test",(req,res)=>{
+    res.status(200) 
+    res.send("this is hello world")
+  });
+
+
+app.listen(3000,()=>{
+    console.log("server is running");
+})
