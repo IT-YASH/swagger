5714e09eba409dc4ce50f38509fa259c2516836b
IT-YASH <pyash7646@gmail.com>
2023-09-23 16:07:24 +0530

    finalised both swagger and app file

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
-const express = require('express')
-const swaggerUI = require('swagger-ui-express')
-const json = require('json')
-const swaggerJsdocs = json.load('./swagger.json')
-const app= express() 
-const port = 3000
-app.use(express.json())      
-app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerJsdocs))
+const express = require("express");
+const swaggerUI = require("swagger-ui-express");
+const json = require("json");
+const swaggerJsdocs = json.load("./swagger.json");
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsdocs));
 
+let users = [{ name: "yash" }, { name: "devansh" }];
 
-
-let users = [
-  {name: "yash patel"},
-  {name: "yash tiwari"}
-];
-
-
-app.get("/test",(req,res)=>{
-  let id = req.body.name;
-  if(req.body){
-    if(!firstName){
-    res.status(400).json({
-    status:"FAIL",
-    toast:"Missing parameter"})
+app.get("/test", (req, res) => {
+  const firstname = users.find((x) => x.name);
+  if (req.body) {
+    if (!firstname) {
+      res.status(400).json({
+        status: "FAIL",
+        toast: "Missing parameter",
+      });
+    } else if (firstname) {
+      res.status(200).json({
+        status: "SUCCESS",
+        firstName: firstname,
+      });
     }
-    else if(firstName){
-    res.status(200).json({
-    status:"SUCCESS",
-    firstName:firstName})
-    }
-  }});
- 
-            
-app.listen(port,()=>{
-    console.log(`http://localhost:${port}`);
-})
+  }
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
