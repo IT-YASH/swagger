0fca052689515b2e8991dfaef39dd3de8aa77216
IT-YASH <pyash7646@gmail.com>
2023-09-23 15:20:13 +0530

    update

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 const express = require('express')
 const swaggerUI = require('swagger-ui-express')
-const YAML = require('yamljs')
-const swaggerJsdocs = YAML.load('./swagger.yamljs')
-const app= express()       
+const json = require('json')
+const swaggerJsdocs = json.load('./swagger.json')
+const app= express() 
+const port = 3000
+app.use(express.json())      
 app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerJsdocs))
 
 
 
+let users = [
+  {name: "yash patel"},
+  {name: "yash tiwari"}
+];
+
+
 app.get("/test",(req,res)=>{
-    res.status(200) 
-    res.send("this is hello world")
-  });
+  let id = req.body.name;
+  if(req.body){
+    if(!firstName){
+    res.status(400).json({
+    status:"FAIL",
+    toast:"Missing parameter"})
+    }
+    else if(firstName){
+    res.status(200).json({
+    status:"SUCCESS",
+    firstName:firstName})
+    }
+  }});
  
             
-app.listen(3000,()=>{
-    console.log("server is running");
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`);
 })
