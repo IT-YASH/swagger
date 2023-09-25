fd4622740b7fdd8198fa915260a766dc597917bb
IT-YASH <pyash7646@gmail.com>
2023-09-25 11:07:48 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const swaggerUI = require("swagger-ui-express");
-const json = require("json");
-const swaggerJsdocs = json.load("./swagger.json");
+const data = require('./swagger.json');
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJsdocs));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(data));
 
 let users = [{ name: "yash" }, { name: "devansh" }];
 
@@ -23,7 +22,7 @@ app.get("/test", (req, res) => {
         firstName: firstname,
       });
     }
-  }
+  } 
 });
 
 app.listen(port, () => {
