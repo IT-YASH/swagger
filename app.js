3022def620b4b07369573f9eac8afcda1ecba09c
IT-YASH <pyash7646@gmail.com>
2023-09-25 15:10:41 +0530

    added a swagger.json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const swaggerUI = require('swagger-ui-express')
 const YAML = require('yamljs')
-const swaggerJsdocs = YAML.load('./swagger.yamljs')
+const swaggerJsdocs = YAML.load('./swagger.yaml')
 const app= express()    
 const port = 3000;   
 app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerJsdocs))
