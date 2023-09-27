2dc35e3fa29662c6fbd08bd28dbb65b6899925c1
IT-YASH <pyash7646@gmail.com>
2023-09-27 11:34:36 +0530

    updated files

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
 const express = require("express");
 const swaggerUI = require("swagger-ui-express");
-const data = require('./swagger.json');
+const data = require("./swagger.json");
 const app = express();
 const port = 3000;
 app.use(express.json());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(data));
 
-// let users = [{name: "yash" }, {name: "devansh" }];
-
-app.get("/test", (req, res) => {
-  // let firstname;
+app.get("/test/:username", (req, res) => {
   if (req.body) {
-    console.log(req.body)
-    let firstname = req.body.username
-    if (!firstname) {
+    let test = req.body.username;
+    if (test) {
+      res.status(200).json({
+        status: "SUCCESS",
+        message: `${test}`,
+      });
+    } else if (!test) {
       res.status(400).json({
         status: "FAIL",
         toast: "Missing parameter",
       });
-    } else if (firstname) {
-      res.status(200).json({
-        status: "SUCCESS",
-        firstname: firstname,
-      });
     }
-  } 
+  }
 });
 
 app.listen(port, () => {
